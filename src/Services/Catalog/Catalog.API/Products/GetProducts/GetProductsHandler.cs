f3969c2fd9d4bcd9b52d507a7a8fe9432183d8d1
jlawas06 <jlawas06@gmail.com>
2024-07-28 21:40:15 +0800

    feat(catalog): get products feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProducstQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+    : IQueryHandler<GetProducstQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProducstQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+        return new GetProductsResult(products);
+    }
+}
