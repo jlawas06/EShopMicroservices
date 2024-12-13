d138f0b07e03ac1bd0c3f5c7421db95670f77edb
John Lloyd Lawas <jlawas06@gmail.com>
2024-12-13 21:15:54 +0800

    feat(basket): register dependencies in Program.cs

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
 
 
 var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.MapGet("/", () => "Hello World!");
 
