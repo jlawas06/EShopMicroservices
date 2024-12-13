9dfbdbc2f2340b4d95eb8c6b0c9ca499d073d6bf
John Lloyd Lawas <jlawas06@gmail.com>
2024-12-13 20:28:14 +0800

    feat(basket): add buildingblock project reference and initial folder structure

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketHandler
+{
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Exceptions.Handler;
+global using FluentValidation;
+global using Basket.API.Models;
